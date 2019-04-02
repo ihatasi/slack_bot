47de82cf4674d097256398ced6a52b014f91f227
ihatasi <ihatasi@gmail.com>
2019-04-02 22:40:02 +0900

    fix string

--- run.py
+++ run.py
@@ -27,18 +27,22 @@ def add_balance(message, money):
     except:
         message.send('please num')
 
-@listen_to('cost (.*)')
-def cost_balance(message, money):
-    try:
-        money = int(money)
-        global balance
-        str1 = 'old balance:' + str(balance) + '\n'
-        balance -= money
-        str2 = 'Cost:' + str(money) + '\n'
-        str3 = 'new balance:' + str(balance) + '\n'
-        message.send(str1+str2+str3)
-    except:
-        message.send('please num')
+@listen_to('cost (.*) (.*)')
+def cost_balance(message, money, obj):
+    if obj == None:
+        message.send('Set obj at now!!!!')
+    else:
+        try:
+            money = int(money)
+            global balance
+            str1 = 'old balance:' + str(balance) + '\n'
+            balance -= money
+            str2 = 'Cost:' + str(money) + '\n'
+            str3 = 'new balance:' + str(balance) + '\n'
+            str4 = 'for ' + str(obj) + '\n'
+            message.send(str1+str2+str3+str4)
+        except:
+            message.send('please num')
 
 @listen_to('balance')
 def send_balance(message):
