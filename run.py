75016710b8c5097a0f2dab2896a8bc8b90491cd6
ihatasi <ihatasi@gmail.com>
2019-04-02 21:54:41 +0900

    fix

--- run.py
+++ run.py
@@ -1,18 +1,14 @@
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
 
 @respond_to('what?')
 def what(message):
     message.reply('free')
+
 @respond_to('say (.*)')
 def say(message, word):
     message.reply(word + '!')
-@respond_to('join (.*) (.*)')
-def say(message, word1, word2):
-    message.reply(word1 + '&' + word2 + '!')
 
-def main():
-    bot = Bot()
-    bot.run()
 
-if __name__ == "__main__":
-    main()
+bot = Bot()
+bot.run()
