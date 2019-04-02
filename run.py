43159d53a1ec6163b6aefb6b5365fdf3a614a5f5
ihatasi <ihatasi@gmail.com>
2019-04-02 21:29:54 +0900

    test

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 from slackbot.bot import Bot\
 
+@respond_to('what?')
+def what(message):
+    message.reply('free')
+@respond_to('say (.*)')
+def say(message, word):
+    message.reply(word + '!')
+@respond_to('join (.*) (.*)')
+def say(message, word1, word2):
+    message.reply(word1 + '&' + word2 + '!')
+
 def main():
     bot = Bot()
     bot.run()
